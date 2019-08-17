70e1cbe6df363109a654829d4b105f2ac1758bba
Dan Park <dpark.sports@gmail.com>
2019-08-16 22:35:19 -0700

    Update BFS.cpp

--- BFS.cpp
+++ BFS.cpp
@@ -3,73 +3,83 @@
 #include <queue>
 #include <string>
 #include <cstdlib>
+#include <vector>
+#include <map>
 
 using namespace std;
-class Node{
+
+class Tree {
 public:
-    int data;
-    Node *left,*right;
-    Node(int d){
-        data=d;
-        left=right=NULL;
-    }
-};
-class BFS{
-private:
-    queue<Node*> levelQueue;
+    map<int,vector<int>> graph;
 
-public:
-    Node* insert(Node* root, int data){
-        if(root==NULL){
-            return new Node(data);
-        }
-        else{
-            Node* cur;
-            if(data<=root->data){
-                cur=insert(root->left,data);
-                root->left=cur;
-            }
-            else{
-                cur=insert(root->right,data);
-                root->right=cur;
-            }
-            return root;
-        }
-    }
-
-
-    void levelOrder(Node * root){
-        if (root == NULL) {
+    void findPath(int a, int b) {
+        vector<int> children = graph[a];
+        if (children.empty()) {
             return;
         }
 
-        cout << root->data << " ";
-
-        if (root->left != NULL) {
-            levelQueue.push(root->left);
-        }
-        if (root->right != NULL) {
-            levelQueue.push(root->right);
+        for (int i = 0; i < children.size(); ++i) {
+            if (children[i] == b) {
+                cout << a << "," << b << endl;
+                break;
+            }
         }
 
-        while (! levelQueue.empty()) {
-            Node *node = levelQueue.front();
-            levelQueue.pop();
-
-            levelOrder(node);
+        for (int j = 0; j < children.size(); ++j) {
+            findPath(children[j], b);
         }
     }
 
-};//End of Solution
+    void insertEdge(int a, int b) {
+        int count = graph.count(a);
+        cout << count;
+
+        vector<int> children = graph[a];
+        if (children.empty()) {
+            vector<int> children;
+            children.push_back(b);
+
+            graph[a] = children;
+
+            cout << endl;
+        } else {
+            vector<int> children = graph[a];
+            children.push_back(b);
+
+            // pass by value, so we copy it again
+            // otherwise, children vector change is localized.
+            graph[a] = children;
+
+            cout << endl;
+        }
+    }
+};
+
+
+
 int main(){
-    BFS myTree;
-    Node* root=NULL;
-    int T,data;
-    cin>>T;
-    while(T-->0){
-        cin>>data;
-        root= myTree.insert(root,data);
+    Tree tree;
+    string edgeString;
+    int count;
+    cin >> count;
+
+    int a, b;
+    while(count-- > 0) {
+        cin >> a;
+        cin >> b;
+
+        tree.insertEdge(a, b);
     }
-    myTree.levelOrder(root);
+
+    int queries;
+    cin >> queries;
+    while(queries-- > 0) {
+        cin >> a;
+        cin >> b;
+
+        tree.findPath(a, b);
+    }
+
+    cout << "a" << endl;
     return 0;
 }
